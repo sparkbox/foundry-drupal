412f8f0abe287c270b1f8b110b697bb3afcfe328
Nathan Rambeck <nathan@heysparkbox.com>
2016-07-27 14:47:01 -0400

    chore: add Platform.sh local settings to settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -711,6 +711,12 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  *
  * Keep this code block at the end of this file to take full effect.
  */
+
+// Automatic Platform.sh settings.
+if (file_exists(__DIR__ . '/settings.platformsh.php')) {
+  include __DIR__ . '/settings.platformsh.php';
+}
+
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
